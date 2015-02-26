059b3ab395dd78aeaeeaf262be63ca3610df4141
Ryan Bartley <ryanbartley@gmail.com>
2015-02-26 16:52:02 -0500

    Fixed default semantic map initialization

--- src/cinder/gl/GlslProg.cpp
+++ src/cinder/gl/GlslProg.cpp
@@ -418,6 +418,8 @@ GlslProg::GlslProg( const Format &format )
 		}
 	}
 	
+    cout << *this << endl;
+    
 	setLabel( format.getLabel() );
 	gl::context()->glslProgCreated( this );
 }
@@ -554,6 +556,9 @@ void GlslProg::cacheActiveUniforms()
 	if( ! mActiveUniformTypesCached ) {
 		GLint numActiveUniforms = 0;
 		glGetProgramiv( mHandle, GL_ACTIVE_UNIFORMS, &numActiveUniforms );
+		
+		auto & semanticNameMap = getDefaultUniformNameToSemanticMap();
+		
 		for( GLint i = 0; i < numActiveUniforms; ++i ) {
 			char name[512];
 			GLsizei nameLength;
@@ -564,8 +569,9 @@ void GlslProg::cacheActiveUniforms()
 			auto loc = glGetUniformLocation( mHandle, name );
 			
 			UniformSemantic uniformSemantic = UniformSemantic::USER_DEFINED_UNIFORM;
-			auto foundSemantic = sDefaultUniformNameToSemanticMap.find( name );
-			if( foundSemantic != sDefaultUniformNameToSemanticMap.end() ) {
+			
+			auto foundSemantic = semanticNameMap.find( name );
+			if( foundSemantic != semanticNameMap.end() ) {
 				uniformSemantic = foundSemantic->second;
 			}
 			
@@ -586,6 +592,8 @@ void GlslProg::cacheActiveAttribs()
 	if( ! mActiveAttribTypesCached ) {
 		GLint numActiveAttrs = 0;
 		glGetProgramiv( mHandle, GL_ACTIVE_ATTRIBUTES, &numActiveAttrs );
+		
+		auto & semanticNameMap = getDefaultAttribNameToSemanticMap();
 		for( GLint i = 0; i < numActiveAttrs; ++i ) {
 			char name[512];
 			GLsizei nameLength;
@@ -596,8 +604,8 @@ void GlslProg::cacheActiveAttribs()
 			auto loc = glGetAttribLocation( mHandle, name );
 			
 			geom::Attrib attributeSemantic = geom::Attrib::NUM_ATTRIBS;
-			auto foundSemantic = sDefaultAttribNameToSemanticMap.find( name );
-			if( foundSemantic != sDefaultAttribNameToSemanticMap.end() ) {
+			auto foundSemantic = semanticNameMap.find( name );
+			if( foundSemantic != semanticNameMap.end() ) {
 				attributeSemantic = foundSemantic->second;
 			}
 			
