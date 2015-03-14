89bd4fbac7fed431b329b72e0005f5b6030d5e1e
Ryan Bartley <ryanbartley@gmail.com>
2015-03-14 17:18:46 -0400

    Added the template versions of uniform buffering
    - Added Type checking
    - Inlined all string variant uniform buffering implementations
    - Added template functions for all variants of uniform buffering

--- src/cinder/gl/GlslProg.cpp
+++ src/cinder/gl/GlslProg.cpp
@@ -32,10 +32,8 @@ using namespace std;
 
 namespace cinder { namespace gl {
     
-
-
-GlslProg::UniformSemanticMap	GlslProg::sDefaultUniformNameToSemanticMap;
-GlslProg::AttribSemanticMap		GlslProg::sDefaultAttribNameToSemanticMap;
+static GlslProg::UniformSemanticMap	sDefaultUniformNameToSemanticMap;
+static GlslProg::AttribSemanticMap	sDefaultAttribNameToSemanticMap;
 
 //////////////////////////////////////////////////////////////////////////
 // GlslProg::Format
@@ -725,6 +723,23 @@ std::string GlslProg::getShaderLog( GLuint handle ) const
 	return log;
 }
 	
+void GlslProg::logMissingUniform( const std::string &name ) const
+{
+	if( mLoggedUniforms.count( name ) == 0 ) {
+		CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
+		mLoggedUniforms.insert( name );
+	}
+}
+	
+void GlslProg::logUniformWrongType( const std::string &name, GLenum uniformType, const std::string &userType ) const
+{
+	if( mLoggedUniforms.count( name ) == 0 ) {
+		CI_LOG_W("Uniform type mismatch for \"" << name << "\", expected "
+				 << gl::constantToString(uniformType) << " and received a " << userType << ".");
+		mLoggedUniforms.insert( name );
+	}
+}
+	
 void GlslProg::setLabel( const std::string &label )
 {
 	mLabel = label;
@@ -935,50 +950,12 @@ void GlslProg::uniform( int location, bool data ) const
 	ScopedGlslProg shaderBind( shared_from_this() );
 	glUniform1i( location, data );
 }
-
-void GlslProg::uniform( const std::string &name, bool data ) const
-{
-	auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_BOOL && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received an integer.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
 	
 // uint32_t
 void GlslProg::uniform( int location, uint32_t data ) const
 {
 	ScopedGlslProg shaderBind( shared_from_this() );
 	glUniform1ui( location, data );
-}
-
-void GlslProg::uniform( const std::string &name, uint32_t data ) const
-{
-	auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_UNSIGNED_INT && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received an integer.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 	
 // uvec2
@@ -988,49 +965,11 @@ void GlslProg::uniform( int location, const uvec2 &data ) const
 	glUniform2ui( location, data.x, data.y );
 }
 
-void GlslProg::uniform( const std::string &name, const uvec2 &data ) const
-{
-	auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_UNSIGNED_INT_VEC2 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received an integer.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // uvec3
 void GlslProg::uniform( int location, const uvec3 &data ) const
 {
 	ScopedGlslProg shaderBind( shared_from_this() );
 	glUniform3ui( location, data.x, data.y, data.z );
-}
-
-void GlslProg::uniform( const std::string &name, const uvec3 &data ) const
-{
-	auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_UNSIGNED_INT_VEC3 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received an integer.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 	
 // uvec4
@@ -1038,25 +977,6 @@ void GlslProg::uniform( int location, const uvec4 &data ) const
 {
 	ScopedGlslProg shaderBind( shared_from_this() );
 	glUniform4ui( location, data.x, data.y, data.z, data.w );
-}
-
-void GlslProg::uniform( const std::string &name, const uvec4 &data ) const
-{
-	auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_UNSIGNED_INT_VEC4 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received an integer.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 	
 // int
@@ -1066,49 +986,11 @@ void GlslProg::uniform( int location, int data ) const
     glUniform1i( location, data );
 }
 
-void GlslProg::uniform( const std::string &name, int data ) const
-{
-	auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_INT && mLoggedUniforms.count( name ) == 0 ) {
-				CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-						 << gl::constantToString(found->mType) << " and received an integer.");
-				mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-    else {
-        if( mLoggedUniforms.count( name ) == 0 ) {
-            CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-            mLoggedUniforms.insert( name );
-        }
-    }
-}
-
 // ivec2
 void GlslProg::uniform( int location, const ivec2 &data ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniform2i( location, data.x, data.y );
-}
-
-void GlslProg::uniform( const std::string &name, const ivec2 &data ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_INT_VEC2 && mLoggedUniforms.count( name ) == 0 ) {
-				CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-						 << gl::constantToString(found->mType) << " and received an ivec2.");
-				mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 // int *, count
@@ -1118,49 +1000,11 @@ void GlslProg::uniform( int location, const int *data, int count ) const
     glUniform1iv( location, count, data );
 }
 
-void GlslProg::uniform( const std::string &name, const int *data, int count ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_INT && mLoggedUniforms.count( name ) == 0 ) {
-				CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-						 << gl::constantToString(found->mType) << " and received an integer.");
-				mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, count );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // ivec2 *, count
 void GlslProg::uniform( int location, const ivec2 *data, int count ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniform2iv( location, count, &data[0].x );
-}
-
-void GlslProg::uniform( const std::string &name, const ivec2 *data, int count ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType == GL_INT_VEC2 && mLoggedUniforms.count( name ) == 0 ) {
-				CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-						 << gl::constantToString(found->mType) << " and received an ivec2.");
-				mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, count );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 // float
@@ -1170,49 +1014,11 @@ void GlslProg::uniform( int location, float data ) const
     glUniform1f( location, data );
 }
 
-void GlslProg::uniform( const std::string &name, float data ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT && mLoggedUniforms.count( name ) == 0 ) {
-				CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-						 << gl::constantToString(found->mType) << " and received a float.");
-				mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // vec2
 void GlslProg::uniform( int location, const vec2 &data ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniform2f( location, data.x, data.y );
-}
-
-void GlslProg::uniform( const std::string &name, const vec2 &data ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_VEC2 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a vec2.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 // vec3
@@ -1222,49 +1028,11 @@ void GlslProg::uniform( int location, const vec3 &data ) const
     glUniform3f( location, data.x, data.y, data.z );
 }
 
-void GlslProg::uniform( const std::string &name, const vec3 &data ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_VEC3 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a vec3.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // vec4
 void GlslProg::uniform( int location, const vec4 &data ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniform4f( location, data.x, data.y, data.z, data.w );
-}
-
-void GlslProg::uniform( const std::string &name, const vec4 &data ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_VEC4 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a vec4.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 // mat3
@@ -1274,49 +1042,11 @@ void GlslProg::uniform( int location, const mat3 &data, bool transpose ) const
     glUniformMatrix3fv( location, 1, ( transpose ) ? GL_TRUE : GL_FALSE, glm::value_ptr( data ) );
 }
 
-void GlslProg::uniform( const std::string &name, const mat3 &data, bool transpose ) const
-{
-	auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_MAT3 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a mat3.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, transpose );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // mat4
 void GlslProg::uniform( int location, const mat4 &data, bool transpose ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniformMatrix4fv( location, 1, ( transpose ) ? GL_TRUE : GL_FALSE, glm::value_ptr( data ) );
-}
-
-void GlslProg::uniform( const std::string &name, const mat4 &data, bool transpose ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_MAT4 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a mat4.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, transpose );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 // Color
@@ -1326,49 +1056,11 @@ void GlslProg::uniform( int location, const Color &data ) const
     glUniform3f( location, data.r, data.g, data.b );
 }
 
-void GlslProg::uniform( const std::string &name, const Color &data ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_VEC3 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a Color(vec3).");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // ColorA
 void GlslProg::uniform( int location, const ColorA &data ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniform4f( location, data.r, data.g, data.b, data.a );
-}
-
-void GlslProg::uniform( const std::string &name, const ColorA &data ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_VEC4 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a ColorA(vec4).");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 // float*, count
@@ -1378,49 +1070,11 @@ void GlslProg::uniform( int location, const float *data, int count ) const
     glUniform1fv( location, count, data );
 }
 
-void GlslProg::uniform( const std::string &name, const float *data, int count ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a float.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, count );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // vec2*, count
 void GlslProg::uniform( int location, const vec2 *data, int count ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniform2fv( location, count, &data[0].x );
-}
-
-void GlslProg::uniform( const std::string &name, const vec2 *data, int count ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_VEC2 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a vec2.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, count );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 // vec3*, count
@@ -1430,49 +1084,11 @@ void GlslProg::uniform( int location, const vec3 *data, int count ) const
     glUniform3fv( location, count, &data[0].x );
 }
 
-void GlslProg::uniform( const std::string &name, const vec3 *data, int count ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_VEC3 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a vec3.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, count );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // vec4*, count
 void GlslProg::uniform( int location, const vec4 *data, int count ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniform4fv( location, count, &data[0].x );
-}
-
-void GlslProg::uniform( const std::string &name, const vec4 *data, int count ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_VEC4 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a vec4.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, count );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 // mat3*, count
@@ -1482,49 +1098,11 @@ void GlslProg::uniform( int location, const mat3 *data, int count, bool transpos
     glUniformMatrix3fv( location, count, ( transpose ) ? GL_TRUE : GL_FALSE, glm::value_ptr( *data ) );
 }
 
-void GlslProg::uniform( const std::string &name, const mat3 *data, int count, bool transpose ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_MAT3 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a mat3.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, count, transpose );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
-}
-
 // mat4*, count
 void GlslProg::uniform( int location, const mat4 *data, int count, bool transpose ) const
 {
     ScopedGlslProg shaderBind( shared_from_this() );
     glUniformMatrix4fv( location, count, ( transpose ) ? GL_TRUE : GL_FALSE, glm::value_ptr( *data ) );
-}
-
-void GlslProg::uniform( const std::string &name, const mat4 *data, int count, bool transpose ) const
-{
-    auto found = findUniform( name );
-	if( found ) {
-		if( found->mType != GL_FLOAT_MAT4 && mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_W("Uniform type mismatch for \"" << found->mName << "\", expected "
-					 << gl::constantToString(found->mType) << " and received a mat4.");
-			mLoggedUniforms.insert( name );
-		}
-		uniform( found->mLoc, data, count, transpose );
-	}
-	else {
-		if( mLoggedUniforms.count( name ) == 0 ) {
-			CI_LOG_E( "Unknown uniform: \"" << name << "\"" );
-			mLoggedUniforms.insert( name );
-		}
-	}
 }
 
 std::ostream& operator<<( std::ostream &os, const GlslProg &rhs )
