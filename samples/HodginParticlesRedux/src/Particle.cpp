5a235460036538b0b3643d02344e34e91752d5b4
Ryan Bartley <ryanbartley@gmail.com>
2015-07-08 13:50:19 -0400

    Fixed error with initialization order, should look at changing it

--- samples/HodginParticlesRedux/src/Particle.cpp
+++ samples/HodginParticlesRedux/src/Particle.cpp
@@ -5,8 +5,8 @@
 using namespace ci;
 
 Particle::Particle( ci::vec3 aLoc, ci::vec3 aVel )
-: mLen( 15 ), mInvLen( 1.0f / (float)mLen ), mLoc( mLen, aLoc ),
-	mVel( aVel ), mAcc( 0 ), mAge( 0.0f ), mAgePer( 1.0f ),
+: mLoc( 15, aLoc ), mColor(), mVel( aVel), mAcc( 0 ), mLen( mLoc.size() ),
+	mInvLen( 1.0f / (float)mLen ), mAge( 0.0f ), mAgePer( 1.0f ),
 	mLifeSpan( ci::Rand::randFloat( 5.0f, 70.0f ) + (ci::Rand::randFloat() < 0.15f ? 100.0f : 0.0f ) ),
 	mRadius( ci::Rand::randFloat( 1.0f, 3.0f ) * (ci::Rand::randFloat() < 0.01f ? 4.0f : 1.0f) ),
 	mMass( mRadius ), mInvMass( 1.0f / mMass ), mCharge( ci::Rand::randFloat( 0.35f, 0.75f ) ),
